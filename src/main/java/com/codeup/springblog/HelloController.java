b7beb88217e80efa50b9c49334b93a4c54e75192
Eva Brechum <Brechum.Eva@gmail.com>
2022-02-22 14:59:14 -0600

    finished homecontroller and mathcontroller exercises

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,29 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    // Use curly braces to establish Path Variable in the naming definition
+    @GetMapping("/hello/{name}") //name references path variable
+    @ResponseBody
+    // Use annotation to get the value of the Path Variable
+    public String hello(@PathVariable String name) {
+        return "Hello, " + name + " !";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+
+    //REQUEST MAPPING
+    @RequestMapping(path = "increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String increment(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + " !";
+    }
+}
