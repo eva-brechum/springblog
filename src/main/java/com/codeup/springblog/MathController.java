ce37305801e723caed6b3cfb0de5cbebab181be5
Eva Brechum <Brechum.Eva@gmail.com>
2022-02-22 23:53:34 -0600

    finished controllers exercises

--- src/main/java/com/codeup/springblog/MathController.java
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -1,35 +1,39 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MathController {
-    @GetMapping("add/{number1}/and/{number2")
+    @RequestMapping(path= "/add/{number1}/and/{number2}", method= RequestMethod.GET)
     @ResponseBody
     ///add/3/and/4
     public int add(@PathVariable int number1, @PathVariable int number2) {
         return number1 + number2;
     }
-
+    //walkthrough solution
+//    public String addition(@PathVariable int num1, @PathVariable int num2){
+//        return "The answer is: " + (num1 + num2);
+//    }
+   // /subtract/3/from/10
     @GetMapping("/subtract/{number1}/from/{number2}")
     @ResponseBody
     public int subtract(@PathVariable int number1, @PathVariable int number2) {
         return number2 -number1;
     }
 
-    @GetMapping("/multiply/{number1}/and{number2}")
+  // /multiply/4/and/5
+    @GetMapping("/multiply/{number1}/and/{number2}")
     @ResponseBody
     public int multiply(@PathVariable int number1, @PathVariable int number2) {
         return number1 * number2;
 
     }
 
-    @GetMapping("divide/{number1}/and{number2")
+   // /divide/6/by/3
+    @GetMapping("divide/{number1}/by/{number2}")
     @ResponseBody
-    public double divide(@PathVariable int number1, @PathVariable int number2) {
-        return (double) number1 / number2;
+    public double divide(@PathVariable double number1, @PathVariable double number2) {
+        return number1 / number2;
     }
 }
---
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -0,0 +1,37 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    //posts index page
+    @GetMapping("/posts")
+    @ResponseBody
+    public String post () {
+        return "posts index page";
+    }
+
+    //view an individual post
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String getPost(@PathVariable long id) {
+        return "view an individual post";
+    }
+    //view the form for creating a post
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewCreateForm() {
+        return "view the form for creating a post";
+    }
+
+    //create a new post
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "create a new page";
+    }
+}
