d462be0fa320f01b553f3fdc1608ddb412c397ea
Eva Brechum <Brechum.Eva@gmail.com>
2022-02-27 15:59:54 -0600

    completed updating the views exercises to create  new post object and pass it to the view

---
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -0,0 +1,55 @@
+package com.codeup.springblog.controllers;
+
+import com.codeup.springblog.models.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class PostController {
+    //posts index page
+    @GetMapping("/posts")
+    public String posts(Model model){
+        List<Post> allThePosts = new ArrayList<>();
+        Post p2 = new Post(2, "Default", "to test if it works");
+        allThePosts.add(p2);
+
+        model.addAttribute("allThePosts" , allThePosts);
+        return "posts/index";
+    }
+//    @ResponseBody
+//    public String post () {
+//        return "posts index page";
+//    }
+
+    //view an individual post
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable long id, Model model) {
+        Post p1 = new Post(1, "Spring is near", "lorem ipsum");
+        model.addAttribute("individualPost" , p1);
+        return "posts/show";
+    }
+//    @ResponseBody
+//    public String getPost(@PathVariable long id) {
+//        return "view an individual post";
+//    }
+    //view the form for creating a post
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewCreateForm() {
+        return "view the form for creating a post";
+    }
+
+    //create a new post
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "create a new page";
+    }
+}
--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -1,11 +1,9 @@
-package controllers;
+package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Random;
 
 @Controller
 public class RollDiceController {
