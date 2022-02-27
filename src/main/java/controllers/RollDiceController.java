94d7710d87f7f99c8220c3d453602cdccb172ca3
Eva Brechum <Brechum.Eva@gmail.com>
2022-02-26 23:42:16 -0600

    working on updates to views exercise

---
+++ src/main/java/controllers/RollDiceController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String rollDiceResults(@PathVariable int n, Model model) {
+
+        int random = (int) (Math.random() * (6 - 1) + 1);
+        model.addAttribute("raandm", random);
+
+        if (n == random) {
+            model.addAttribute("you guessed wrong", "guess");
+        }else{ model.addAttribute("You rolled", "correctly");
+//
+        }
+        return "roll-dice";
+    }
+}
