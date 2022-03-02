d2f4ef5e1e1b14ce3abaea1359e9ebfe76b14ebf
Eva Brechum <Brechum.Eva@gmail.com>
2022-03-01 22:48:44 -0600

    working on services exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -3,6 +3,8 @@ package com.codeup.springblog.controllers;
 import com.codeup.springblog.models.Post;
 import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.UserRepository;
+import com.codeup.springblog.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +17,18 @@ public class PostController {
     private PostRepository postsDao;
     private UserRepository usersDao;
 
-    public PostController(PostRepository postsDao, UserRepository usersDao) {
+    @Autowired
+    private final EmailService emailService;
+
+
+    public PostController(PostRepository postsDao, UserRepository usersDao, EmailService emailService) {
         this.postsDao = postsDao;
         this.usersDao = usersDao;
+        this.emailService = emailService;
+
     }
+
+
 
     //posts index page
     @GetMapping("/posts")
@@ -84,6 +94,14 @@ public class PostController {
     public String delete(@PathVariable long id) {
         postsDao.deleteById(id);
         return "redirect:/posts";
+    }
+
+    @GetMapping("/send-email")
+    public String sendEmail() {
+        String emailSubject = "Testing email subject";
+        String emailBody = "Body of email test";
+        emailService.prepareAndSend();
+        return "redirect:/";
     }
 
 }
