d7825ac05c5ddcf7c23421131f82da9cbf3dd8c2
Eva Brechum <Brechum.Eva@gmail.com>
2022-03-02 23:11:24 -0600

    finished sign up page

--- src/main/java/com/codeup/springblog/SecurityConfiguration.java
+++ src/main/java/com/codeup/springblog/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package com.codeup.springblog;
 
 import com.codeup.springblog.services.UserDetailsLoader;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
